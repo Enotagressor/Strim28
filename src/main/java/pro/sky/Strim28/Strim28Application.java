f4f811972948f63e3e260d5a2c749130308fbaaa
Sergey <enotagressor@gmail.com>
2022-07-05 23:46:39 +0700

    Initial commit

---
+++ src/main/java/pro/sky/Strim28/Strim28Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Strim28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Strim28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Strim28Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Strim28/Strim28ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Strim28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Strim28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
